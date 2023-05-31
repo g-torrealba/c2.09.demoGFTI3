322180320f070c58575372c09a5fbc4b0fdc7141
Giovanni Torrealba <47801129+g-torrealba@users.noreply.github.com>
2023-05-31 22:33:12 +0000

    Cambio.3

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 import "./style.css";
 
 document.querySelector("#app").innerHTML = `
-  Éxito c2.09.demoGFTI3
-  Giovanni Torrealba
+  <p>Éxito c2.09.demoGFTI3</p>
+  <p>Giovanni Torrealba</p>
 `;
 
 setupCounter(document.querySelector("#counter"));
