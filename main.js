cb2d72b9e1952d32ec33a2648f78c37366c68df9
Giovanni <g_torrealba@ucla.edu.ve>
2023-05-31 18:36:25 -0400

    Cambio.3

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import "./style.css";
 
 document.querySelector("#app").innerHTML = `
   <p>Éxito c2.09.demoGFTI3</p>
-  <p>Giovanni Torrealba</p>
+  <p>ÉXITO Giovanni Torrealba ÉXITO</p>
 `;
 
 setupCounter(document.querySelector("#counter"));
