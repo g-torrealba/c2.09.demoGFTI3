93d62b3d8703cbccfe0ae456df3a2af7ac54a1c3
Giovanni Torrealba <47801129+g-torrealba@users.noreply.github.com>
2023-05-31 22:32:15 +0000

    Cambio.2

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
 import "./style.css";
-import javascriptLogo from "./javascript.svg";
-import viteLogo from "/vite.svg";
-import { setupCounter } from "./counter.js";
 
 document.querySelector("#app").innerHTML = `
   Éxito c2.09.demoGFTI3
+  Giovanni Torrealba
 `;
 
 setupCounter(document.querySelector("#counter"));
